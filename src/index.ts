44038bf9c672dcc6c401fb1af56adfc72c51d09d
Jesus Alvarez <jesusalvarezaguilar94@gmail.com>
2023-08-05 15:57:22 +0200

    Add Main and footer section

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-import { Header } from './components/header/header.ts';
+import { Footer } from './components/footer/footer.js';
+import { Header } from './components/header/header.js';
+import { Main } from './components/main/main.js';
 
-const webPageComponents = [new Header('#root')];
+const webPageComponents = [
+	new Header('#root'),
+	new Main('#root'),
+	new Footer('#root'),
+];
